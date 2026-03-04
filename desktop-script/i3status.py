8eb926311092b4dbd50ee6c3f23536329f9c9b2c
Ram <ram@acer-laptop.pr>
2026-03-04 09:54:15 +0800

    i3status.py

--- desktop-script/i3status.py
+++ desktop-script/i3status.py
@@ -11,6 +11,7 @@ import cpu
 import battery
 import mpv_ctrl
 import wallpaper
+import os
 from other import fill
 
 
@@ -23,8 +24,8 @@ class statustime(dict):
         fill(self,time.strftime("%Y-%m-%d %X",tm),time.strftime("%X",tm),None)
 
 
-def run(logout):
-    cols=(cpu.Cpu(),mpv_ctrl.MpvControl(),cpu.Mem(),netadapt.Net(),battery.Battery(),wallpaper.Wallpaper('/home/com/wallpaper/',logout),statustime())
+def run(logout,cfg):
+    cols=(cpu.Cpu(),mpv_ctrl.MpvControl(),cpu.Mem(),netadapt.Net(),battery.Battery(),wallpaper.Wallpaper(cfg['wallpaper'],logout),statustime())
     for idx in range(len(cols)):
         cols[idx]['name']='n{}'.format(idx)
     version()
@@ -34,18 +35,40 @@ def run(logout):
             col.update()
         print(json.dumps(cols,ensure_ascii=False),end=",\n")
         time.sleep(5)
-
+def config(cfgname):
+    try:
+        with open(cfgname) as cfg:
+            lines=cfg.readlines()
+    except Exception as e:
+        return e
+    cfg={}
+    for li in lines:
+        key,val=li.split('=')
+        cfg[key]=val.strip()
+    return cfg
 def version():
 #    要sway-bar响应鼠标事件，要修改以下内容
 #    print('{"version":1,"click_events":true}')
     print('{"version":1}')
     
 if __name__ == "__main__":
-    logname='/tmp/swb-input.log'
+    cfgname=os.environ['HOME']+'/git/linux/desktop-script/i3status.cfg'
+    cfg=config(cfgname)
+    cfgerr=False
+    if isinstance(cfg,Exception):
+        logname='/tmp/sway-bar.log'
+        cfgerr=True
+    else:
+        if 'log' in cfg:
+            logname=cfg['log']
+        else:
+            logname='/tmp/sway-bar.log'
     with open(logname,'w',buffering=1) as log:
-        tak=threading.Thread(target=run,args=(log,))
+        tak=threading.Thread(target=run,args=(log,cfg))
         tak.start()
         print('sway-bar log start',file=log)
+        if cfgerr:
+            print(f'open file {cfgname} error',file=log)
         #log.flush()
         cnt=0
         while True:
