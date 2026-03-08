750fed3801f701bbea00d6be6de9ca70c8449125
Mr. Copp <copper@desktop.home>
2026-03-08 12:06:50 +0800

    octave

--- txt/bexpense.py
+++ txt/bexpense.py
@@ -9,7 +9,7 @@ class Expense:
     def __init__(self,dbname):
         with open(dbname) as fh:
             lines=fh.readlines()
-        self.__db=[]
+        self.__db=list()
         for li in lines:
             if len(li.strip())==0:continue
             row=li.split()
@@ -17,12 +17,15 @@ class Expense:
             if cnt != 4:
                 raise Exception(f'unexpect cols{cnt} in line[{li}]')
             self.__db.append(parse(row))
+        self.__db.sort(key=lambda da:da[0],reverse=True)
     def save(self):
         sum=0
         for rec in self.__db:
             print(f'{rec[0]} {rec[1]:8.2f} {rec[2]} {rec[3]}')
             sum = sum + rec[1]
         print(f'total ￥{sum:.2f}')
+    def search(self,scope=(datetime.datetime(2025,1,1),datetime.datetime.now())):
+        pass
 if __name__=="__main__":
     a=Expense('big-expense.txt')
     a.save()
